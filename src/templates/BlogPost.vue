7977d631a751b98187394a0561e371ea7451fb6c
Johan Dahl <johan.devotee@gmail.com>
2020-05-18 07:56:37 +0200

    Added editor and syntax rules
    Its a mess, but it works

--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -1,29 +1,29 @@
 <template>
-  <div>
-    <Header></Header>
-    <GridDefault>
-      <div class="post">
-        <g-link to="/blog" class="go-back">Go Back</g-link>
-        <PostMeta
-          :date="$page.post.date"
-          :timeToRead="$page.post.timeToRead"
-          :author="$page.post.author"
-        />
-        <h1 class="post-title">{{ $page.post.title }}</h1>
-        <div class="post-content" v-html="$page.post.content"></div>
-      </div>
-    </GridDefault>
-  </div>
+    <div>
+        <Header></Header>
+        <GridDefault>
+            <div class="post">
+                <g-link to="/blog" class="go-back">Go Back</g-link>
+                <PostMeta
+                    :date="$page.post.date"
+                    :timeToRead="$page.post.timeToRead"
+                    :author="$page.post.author"
+                />
+                <h1 class="post-title">{{ $page.post.title }}</h1>
+                <div class="post-content" v-html="$page.post.content"></div>
+            </div>
+        </GridDefault>
+    </div>
 </template>
 
 <script>
-import PostMeta from "@/components/PostMeta";
+import PostMeta from '@/components/PostMeta'
 
 export default {
-  components: {
-    PostMeta
-  }
-};
+    components: {
+        PostMeta,
+    },
+}
 </script>
 
 <page-query>
@@ -41,27 +41,27 @@ export default {
 
 <style lang="scss">
 .post {
-  grid-area: content;
+    grid-area: content;
 
-  > * + * {
-    margin-top: 2rem;
-  }
+    > * + * {
+        margin-top: 2rem;
+    }
 }
 
 .post-content {
-  * + {
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-      margin-top: 4rem;
+    * + {
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+            margin-top: 4rem;
+        }
     }
-  }
 }
 
 .go-back {
-  font-size: 0.9rem;
+    font-size: 0.9rem;
 }
 </style>
